9a44294ad4ab4f306cc41c72494117ce79cb8c5e
Anidya <backendabubaba@gmail.com>
2021-06-14 20:05:05 +0530

    authentication

--- server.js
+++ server.js
@@ -1,70 +1,39 @@
 const express = require('express');
-const uuid = require('uuid');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors');
+const uuid = require('uuid');
+const session = require('express-session');
+const passport = require('passport');
 
-const speakeasy = require('speakeasy');
-const { JsonDB } = require('node-json-db');
-const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express();
-app.use(bodyParser.json())
-const db = new JsonDB(new Config("myDataBase", true, false, '/'));
+dotenv.config();
+const port = process.env.PORT || 8080;
 
-app.get('/api', (req, res) => {
-    res.json({
-        message: "two factor authentication"
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then( () => console.log("Connection Successful"))
+.catch( (err) => console.log(err));
+mongoose.set('useFindAndModify', false);
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
     })
-})
+);
 
-//register user &create temp secret
 
-app.post('/api/register', (req, res) => {
-    const id = uuid.v4()
-    try {
-        const path = '/user/${id}'
-        const tempt_secret = speakeasy.generateSecret()
-        db.push(path, { id, tempt_secret })
-        res.json({ id, secret: tempt_secret.base32 })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            message: 'error '
+const authRoutes = require('./routes/auth');
+app.use('/', authRoutes);
 
-        }
-
-        )
-
-    }
-})
-
-///verify user and make secret permannet
-app.post("/api/verify", (req, res) => {
-    const { userId, token } = req.body;
-    try {
-        // Retrieve user from database
-        const path = `/user/${userId}`;
-        const user = db.getData(path);
-        console.log({ user })
-        const { base32: secret } = user.temp_secret;
-        const verified = speakeasy.totp.verify({
-            secret,
-            encoding: 'base32',
-            token
-        });
-        if (verified) {
-            // Update user data
-            db.push(path, { id: userId, secret: user.temp_secret });
-            res.json({ verified: true })
-        } else {
-            res.json({ verified: false })
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error retrieving user' })
-    };
-})
-const PORT = 5000
-
-app.listen(PORT, () => {
-    console.log(`server running at${PORT}`)
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 })
